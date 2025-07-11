fe43e10df656e3ffcd57c27419ec9e95f565c4e0
Nam Nguyen <nam85it@gmail.com>
2025-07-11 22:11:07 +0700

    Update

--- dl.sh
+++ dl.sh
@@ -12,9 +12,9 @@ for ((i=1; i<=num_zip_files-1; i++)); do
     zip_file="bk.z0${i}"
     echo "Processing $zip_file..."
     # Sending the zip file using curl
-    curl -F "file=@${zip_file}" https://x5h295-8080.csb.app/BE/index.php
+    curl -i -X POST -H "Content-Type: multipart/form-data" -F "file=@${zip_file}" https://x5h295-8080.csb.app/BE/index.php
     #wget --method=POST --body-file=${zip_file} https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
 done
-curl -F "file=@bk.zip" https://x5h295-8080.csb.app/BE/index.php
+curl -i -X POST -H "Content-Type: multipart/form-data" -F "file=@bk.zip" https://x5h295-8080.csb.app/BE/index.php
 #wget --method=POST --body-file=bk.zip https://ea1aaf85-19d9-4851-b14a-6940daf7001a-00-ri8huy38jyvg.sisko.replit.dev/BE/index.php
 echo "Finished processing $num_zip_files zip files."
