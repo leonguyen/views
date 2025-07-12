2a4dcaed6b928b41926865c21fc115c474d342e4
Nam Nguyen <nam85it@gmail.com>
2025-07-12 16:52:09 +0700

    Update

---
+++ tn.sh
@@ -0,0 +1,17 @@
+tar -czf bk.tar.gz *
+curl -XPOST "https://api.transfernow.net/v1/transfers" \
+  -H "Content-Type: application/json" \
+  -H "x-api-key: aad98a60-138a-4aec-a1d4-868fe9f2445a" \
+  -d '{
+        "langCode": "en",
+        "toEmails": ["my.contact@provider.com", "my.other.contact@provider.com"],
+        "files": [{
+          "name": "bk.tar.gz"
+        }],
+        "message": "A brillant transfer content, thank you",
+        "subject": "BK",
+        "validityStart": "2022-10-28T08:52:25.955Z",
+        "validityEnd": "2022-11-01T08:52:25.955Z",
+        "allowPreview": true,
+        "maxDownloads": 7
+      }'
