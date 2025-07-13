f46c3c9aaf4e95a10c7ec30cadb0425c0e6774d0
Nam Nguyen <nam85it@gmail.com>
2025-07-13 10:27:30 +0700

    Update

--- ca.sh
+++ ca.sh
@@ -1,4 +1,4 @@
 curl -X POST https://v2.convertapi.com/convert/any/to/zip \
  -H "Authorization: Bearer BG8iXBvMv5VwJpjQXoQx2VooMb0arawG" \
- -F "Files[0]=@/bk.tar.gz" \
+ -F "Files[0]=@bk.tar.gz" \
  -F "StoreFile=true" 
